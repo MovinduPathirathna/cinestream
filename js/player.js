34f5b04ee0012f3875e633db26db5c88ad8c3510
MovinduPathirathna <pathirathnalsmm@gmail.com>
2026-09-12 00:49:35 +0530

    Add files via upload

--- js/player.js
+++ js/player.js
@@ -63,17 +63,9 @@ import { API } from './api.js';
 // Servers ordered by cleanliness (fewest ads first)
 export const SERVERS = [
     {
-        id: 'vidsrcto',
-        name: 'VidSrc.to',
-        badge: 'Recommended',
-        hasSubs: true,
-        getMovieUrl: (id) => `https://vidsrc.to/embed/movie/${id}`,
-        getTvUrl:    (id, s, e) => `https://vidsrc.to/embed/tv/${id}/${s}/${e}`
-    },
-    {
         id: 'autoembed',
-        name: 'AutoEmbed',
-        badge: 'Clean',
+        name: 'AutoEmbed (Clean)',
+        badge: 'Recommended',
         hasSubs: true,
         getMovieUrl: (id) => `https://autoembed.cc/movie/tmdb/${id}`,
         getTvUrl:    (id, s, e) => `https://autoembed.cc/tv/tmdb/${id}-${s}-${e}`
@@ -93,6 +85,14 @@ export const SERVERS = [
         hasSubs: true,
         getMovieUrl: (id) => `https://www.embed3.me/embed/movie/${id}`,
         getTvUrl:    (id, s, e) => `https://www.embed3.me/embed/tv/${id}/${s}/${e}`
+    },
+    {
+        id: 'vidsrcto',
+        name: 'VidSrc.to',
+        badge: 'CC',
+        hasSubs: true,
+        getMovieUrl: (id) => `https://vidsrc.to/embed/movie/${id}`,
+        getTvUrl:    (id, s, e) => `https://vidsrc.to/embed/tv/${id}/${s}/${e}`
     },
     {
         id: 'multiembed',
